dcaa8df782008179e0927a3d99f5cf54525b0397
SantiagoFigueroa1017 <msfigueroam@ufpso.edu.co>
2024-09-17 17:19:51 -0500

    Creacion del metodo Crear()

--- src/empresas/EmpresaS.java
+++ src/empresas/EmpresaS.java
@@ -1,5 +1,6 @@
 package empresas;
 
+import empresas.services.DireccionService;
 import empresas.services.EmpleadoService;
 
 public class EmpresaS {
@@ -8,6 +9,9 @@ public class EmpresaS {
     public static void main(String[] args) {
         EmpleadoService service = new EmpleadoService();
         service.menu();
+
+        DireccionService direccion = new DireccionService();
+        direccion.crear();
     }
     
 }
--- src/empresas/services/DireccionService.java
+++ src/empresas/services/DireccionService.java
@@ -12,19 +12,34 @@ public class DireccionService {
 
     public DireccionService() {
         sc = new Scanner(System.in);
-
     }
 
     public Direccion crear(){
         System.out.println("--CREAR DIRECCION--");
-        System.out.println("ingrese numero de calle");
-        System.out.println("ingrese numero de casa");
 
+        System.out.println("Ingrese el número de calle");
+        String calle = sc.next();
 
+        System.out.println("Ingrese el número de la casa");
+        String numeroCasa = sc.next();
+
+        System.out.println("Ingrese el barrio");
+        String barrio = sc.next();
+
+        System.out.println("Ingrese la ciudad");
+        String ciudad = sc.next();
+
+        System.out.println("Ingrese el codigo Postal");
+        String codigoPostal = sc.next();
+
+        Direccion direccion = new Direccion(calle, numeroCasa, barrio, ciudad, codigoPostal);
+
+        return direccion;
     }
 
-    public String visualizar(){
 
-    }
+//    public String visualizar(){
+//
+//    }
 
 }
